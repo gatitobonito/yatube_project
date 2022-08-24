1d891d96d547ef67f1b848b1058cb7920135ff3f
tepikin_da <tipdim@gmail.com>
2022-08-24 23:55:37 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница для постов;
+def group_posts(request, slug):
+    return HttpResponse(f'Страница для общих постов {slug}') 
