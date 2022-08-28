88538df355a3a60608d9e5dab0d941b5346b4701
tepikin_da <tipdim@gmail.com>
2022-08-28 23:32:24 +0300

    Добавление шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,14 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 # Страница для постов;
 def group_posts(request, slug):
-    return HttpResponse(f'Страница для общих постов {slug}') 
+    return HttpResponse(f'Страница для общих постов {slug}')
